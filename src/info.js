972c8d90b510a6bb01f18eef8a7a9b8d370b38bc
magnossant <164401216+magnossant@users.noreply.github.com>
2024-08-27 18:42:37 -0300

    Update info.js

--- src/info.js
+++ src/info.js
@@ -1,9 +1,9 @@
 const debug = require('debug')('bot:info');
 module.exports = {
-    botName: 'Giulia',
-    dev: 'Joaquim',
+    botName: 'Alcântara',
+    dev: 'S.Santos',
     botFoto: 'https://img96.pixhost.to/images/770/483545018_4546.jpg',
-    nDev: 'wa.me/557481033040',
+    nDev: 'wa.me/447407405401',
     prefix: '!',
     admin: 'Indefinido'
 };
